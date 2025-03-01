fa8b704de591068744d3f7a1248494f738d1dcff
Trace-Gary <tracegary@gmail.com>
2025-03-01 10:59:46 -0600

    Updated

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import HomePage from './components/HomePage';
 import Footer from './components/Footer';
-import Shop from './components/Shop'; // You can create this component later
-import AboutUs from './components/AboutUs'; // Create this component
-import ContactUs from './components/ContactUs'; // Create this component
+import ShopPage from './Pages/ShopPage';
+import AboutUs from './Pages/AboutUs';
+import ContactUs from './Pages/ContactUs';
+import HomePage from './Pages/HomePage';
+import Filter from './components/Filter';
+
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <Header/>
   );
 };
 
