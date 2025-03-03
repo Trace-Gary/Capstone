665f9c7ef221a5038eb102476670ea8267953bf7
Trace-Gary <tracegary@gmail.com>
2025-03-03 16:13:27 -0600

    Deleted unnecessary photos and addedmorecomponents

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import HomePage from './Pages/HomePage';
 import ShopPage from './Pages/ShopPage';
 import AboutUs from './Pages/AboutUs';
 import ContactUs from './Pages/ContactUs';
-import HomePage from './Pages/HomePage';
-import Filter from './components/Filter';
+
 
 
 const App = () => {
   return (
-    <Header/>
+    <div className="App">
+    <HomePage/>
+    <ShopPage/>
+    <AboutUs/>
+    <ContactUs/>
+    </div>
   );
 };
 
